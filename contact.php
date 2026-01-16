83a03b28409ee96572e5f6725f8636f0e05fc462
Ahndre T. R. Walters <waltersahndre@gmail.com>
2026-01-16 09:04:23 -0400

    Update contact.php

---
+++ contact.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Contact | CybAware</title>
+    <link rel="stylesheet" href="css/styles.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
+</head>
+<body>
+    <div class="container">
+
+        <?php include 'includes/navigation.php'; ?>
+
+        <div class="main-content">
+            
+        </div>
+        <?php include 'includes/footer.php'; ?>
+        </div>
+        
+        <div class="menu-overlay" id="menuOverlay"></div>
+
+    </div>
+
+    <script src="js/navigation.js"></script>
+</body>
+</html>
