d03dbe2cc97fe4ca4f4e7240890493ac2b964473
Ahndre T. R. Walters <waltersahndre@gmail.com>
2026-01-15 21:04:30 -0400

    Update

---
+++ includes/navigation.php
@@ -0,0 +1,27 @@
+<?php
+// Navigation Component for CybAware
+?>
+
+<!-- Navigation -->
+<nav>
+    <div class="logo">
+        <a href="index.php">
+            <div class="logo-text">CybAware</div>
+        </a>
+    </div>
+    
+    <ul class="nav-links" id="navLinks">
+        <li><a href="about.php">About</a></li>
+        <li><a href="team.php">Team</a></li>
+        <li><a href="game.php">Game</a></li>
+        <li><a href="certificate.php">Certificate</a></li>
+        <li><a href="contact.php">Contact</a></li>
+    </ul>
+    
+    <div class="mobile-menu-btn" id="mobileMenuBtn">
+        <i class="fas fa-bars"></i>
+    </div>
+</nav>
+
+<!-- Mobile Menu Overlay -->
+<div class="menu-overlay" id="menuOverlay"></div>
