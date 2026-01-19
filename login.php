965c68cfbab924112a96802ed30633c511dfa6b3
Ahndre T. R. Walters <waltersahndre@gmail.com>
2026-01-18 20:44:25 -0400

    Update login.php

--- login.php
+++ login.php
@@ -53,7 +53,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
             if(mysqli_stmt_execute($stmt)){
                 mysqli_stmt_store_result($stmt);
                 
-                if(mysqli_stmt_num_rows($stmt) == 1){                    
+                if(mysqli_stmt_num_rows($stmt) == 1){
                     mysqli_stmt_bind_result($stmt, $id, $first_name, $last_name, $email, $hashed_password);
                     if(mysqli_stmt_fetch($stmt)){
                         if(password_verify($password, $hashed_password)){
