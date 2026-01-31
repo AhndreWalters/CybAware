9461342ab8c639ff6df9e92e1486393c37bb7005
Ahndre T. R. Walters <waltersahndre@gmail.com>
2026-01-31 15:58:39 -0400

    Create CybAware-DB.sql

---
+++ sql/CybAware-DB.sql
@@ -0,0 +1,15 @@
+CREATE DATABASE CybAwareDB;
+
+USE CybAwareDB;
+
+CREATE TABLE users (
+    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    first_name VARCHAR(50) NOT NULL,
+    last_name VARCHAR(50) NOT NULL,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    INDEX idx_first_name (first_name),
+    INDEX idx_last_name (last_name),
+    INDEX idx_full_name (first_name, last_name)
+);
