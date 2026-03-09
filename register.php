9050b9c9efef180acb771a9992d35881805e5130
Ahndre T. R. Walters <waltersahndre@gmail.com>
2026-03-08 22:08:22 -0400

    Update register.php

--- register.php
+++ register.php
@@ -97,7 +97,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="shortcut icon" href="images/ui-icon-social-engineering.png" type="image/x-icon">
+    <link rel="shortcut icon" href="images/cybawarelogo.png" type="image/x-icon">
     <title>Sign Up | CybAware</title>
     <link rel="stylesheet" href="css/styles.css">
 </head>
