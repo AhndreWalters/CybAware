1d96306e33e1d0069681fba6f170a5c00877088d
Ahndre T. R. Walters <waltersahndre@gmail.com>
2026-03-17 21:59:54 -0400

    Update footer.php

--- includes/footer.php
+++ includes/footer.php
@@ -1,13 +1,25 @@
 <?php
+// This file is the footer that gets included at the bottom of every page on the site
 ?>
 
+<?php // Outer wrapper div that holds the entire footer section ?>
 <div class="simple-footer">
+
+    <?php // Inner div that centres and organises the footer content ?>
     <div class="footer-content">
+
+        <?php // Shows the copyright text with the company name and year ?>
         <div class="copyright">
             © 2026 CybAware
         </div>
+
+        <?php // List of footer navigation links ?>
         <ul class="footer-links">
+
+            <?php // Link that takes the user to the Terms and Conditions page ?>
             <li><a href="terms.php">Terms</a></li>
+
         </ul>
+
     </div>
 </div>
