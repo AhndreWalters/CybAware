46b708bf1f77451fc6771f341075d4eeb2b7b731
Ahndre T. R. Walters <waltersahndre@gmail.com>
2026-03-17 22:19:43 -0400

    Update logout.php

--- logout.php
+++ logout.php
@@ -1,10 +1,14 @@
 <?php
+// Start the session so we can access and clear the current session data
 session_start();
- 
+
+// Wipe all session variables so the user's login details are completely removed
 $_SESSION = array();
- 
+
+// Destroy the session itself so it no longer exists on the server
 session_destroy();
- 
+
+// Send the user back to the login page now that they are logged out
 header("location: login.php");
 exit;
 ?>
